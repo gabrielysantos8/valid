b5ce385a4184519054d88afc0fb9e2cb4cdf6d40
gabrielysantos8 <gabrielysantosp7@gmail.com>
2026-05-31 17:02:50 -0300

    menu responsivo

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,24 @@
 import Header from "./components/Header";
+import "./style.css";
 
 function Home() {
   return (
     <>
       <Header />
+
+      <section className="fundoverde">
+        <div className="row">
+
+          <div className="col-6">
+            <p className="text-branco">Verifique a autenticidade de documentos com <p className="text-subli">Inteligência Artificial</p></p>
+          </div>
+
+          <div className="col-6">
+            img
+          </div>
+
+        </div>
+      </section>
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,21 @@
 import { Link } from "react-router-dom";
 import { IoSearch } from "react-icons/io5";
 import { FaRegUser } from "react-icons/fa6";
-import "./style.css";
+import "./compostyle.css";
+
+import { FaBars } from "react-icons/fa";
+import { useState } from "react";
 
 function Header() {
+    const [menuAberto, setMenuAberto] = useState(false);
+
     return(
         <header>
             <div>
                 <h1>Logo</h1>
             </div>
-
-            <nav>
+            
+            <nav className={menuAberto ? "ativo" : ""}>
                 <ul>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/">Sobre</Link></li>
@@ -20,10 +25,21 @@ function Header() {
                 </ul>
             </nav>
 
-            <div className="icons">
-                <IoSearch />
-                <FaRegUser />
+            <div className="actions">
+                <div className="icons">
+                    <IoSearch />
+                    <FaRegUser />
+                </div>
+
+                <button
+                    className="menu-btn"
+                    onClick={() => setMenuAberto(!menuAberto)}
+                >
+                    <FaBars />
+                </button>
             </div>
+
+
         </header>
     );
 }
